896f988a2e0ca86678558d5cfdfe8dcbf9374329
Filipe <filipe.raupp@hotmail.com>
2017-09-27 20:49:03 -0300

    parenteses no getSenha();

---
+++ src/SistemaSuporte/PessoaDao.java
@@ -0,0 +1,60 @@
+package SistemaSuporte;
+
+import JavaDb.*;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ * Created by filip on 27/09/2017.
+ */
+public class PessoaDao {
+
+    JavaDb.DbHelper sqlite;
+    public PessoaDao(){
+        sqlite = new JavaDb.DbHelper();
+    }
+
+    public void save(Pessoa pessoa){
+
+        if(a!=null){
+            String sql= "INSERT INTO ALUNO (NOME,IDADE,CODIGO,SALARIO) " +
+                    "VALUES ('"+pessoa.getId()+"','"+pessoa.getNome()+"', "+pessoa.getTelefone()+", '"+pessoa.getEmail()+"', '"+pessoa.getLogin()+"', '"+pessoa.getSenha()+"' );";
+            sqlite.executarSQL(sql);
+        }
+
+    }
+
+    public ArrayList<Aluno> list (){
+        ArrayList<Aluno> lista= new ArrayList<>();
+        String sql= "SELECT * FROM ALUNO";
+        ResultSet rs= sqlite.querySql(sql);
+        try {
+            while (rs.next()){
+                long id = rs.getLong("ID");
+                String  name = rs.getString("NOME");
+                int age  = rs.getInt("IDADE");
+                String  codigo = rs.getString("CODIGO");
+                double salary = rs.getDouble("SALARIO");
+                System.out.println( "ID = " + id );
+                System.out.println( "NOME = " + name );
+                System.out.println( "IDADE = " + age );
+                System.out.println( "ENDERECO = " + codigo );
+                System.out.println( "SALARIO = " + salary );
+                System.out.println();
+                Aluno a =new Aluno(id,name,age,codigo,salary);
+                lista.add(a);
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        if(lista.isEmpty()) {
+            return null;
+        }else {
+            return lista;
+
+        }
+
+
+    }
+}
