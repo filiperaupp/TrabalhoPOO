9bdb06facf4eb30cc4099bfc8f86aceef9a8e29d
Luccasfs98 <luccasfs98@gmail.com>
2017-09-28 11:18:03 -0300

    Criando o main...
    tela de cliente
    cria chamado
    edita chamado
    cancela chamado
    
    luccas

--- src/SistemaSuporte/Chamado.java
+++ src/SistemaSuporte/Chamado.java
@@ -4,4 +4,22 @@ public class Chamado {
     private String nome;
     private String descricao;
     private int nivelPrioridade; // 1- baixa 2- moderada, 3-alta, 4-urgente
+
+    public boolean criaChamado(){
+        //Cria chamado
+        return true;
+    };
+
+    public Chamado procuraChamado(Chamado chamado,String nome){
+        //procura chamado pelo nome
+        return chamado;
+    }
+
+    public void editarChamado(){
+
+    }
+
+    public void cancelarChamado(){
+
+    }
 }
--- src/SistemaSuporte/Main.java
+++ src/SistemaSuporte/Main.java
@@ -6,17 +6,72 @@ public class Main {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
         int ope;
+        int operacao;
         Gerente admin = new Gerente();
 
         do {
 
             System.out.println("Sistema de Chamados...");
             System.out.println("Login: ");
-            login = tc.next();
+            login = tc.next(); // Vai pesquisar no banco se já existe esse login e senha
             System.out.println("Senha: ");
             senha = tc.next();
 
-            if ()
+            if (pessoa.identificador == 100){ // se o identificador for 100 quer dizer que é um cliente
+                do {
+                    System.out.println("Cliente " + pessoa.nome+ "está logado no sistema...");
+                    System.out.println("Escolha uma opção: ");
+
+                    System.out.println("1-Abrir Chamado");
+                    System.out.println("2-Editar Chamado");
+                    System.out.println("3-Mostrar meus Chamados");
+                    System.out.println("4-Sair da conta");
+                    operacao = tc.nextInt();
+
+                    switch(operacao){
+                        case 1:
+                            System.out.println("Criando Chamado...");
+                            Chamado chamado = new Chamado();
+
+                            if(chamado.criaChamado()){
+                                System.out.println("Chamado criado com Sucesso!");
+                            }   else {
+                                System.out.println("O Chamado não foi criado com Sucesso!");
+                                }
+                        break;
+
+                        case 2:
+                            System.out.println("Editando Chamado...");
+                            System.out.println("Digite o nome do chamado: ");
+                            String nomeChamado;
+                            nomeChamado = tc.next();
+                            Chamado editar = chamado.procuraChamado(chamado, nomeChamado);
+
+                            System.out.println("O que você deseja fazer? ");
+                            System.out.println("1-Editar");
+                            System.out.println("2-Cancelar Chamado");
+                            System.out.println("3-Voltar");
+                            int edita;
+                            edita = tc.nextInt();
+                            if(edita==1){
+                                editar.editarChamado();
+                                break;
+                            }   else if(edita==2){
+                                editar.cancelarChamado();
+                                break;
+                                }
+                                else break;
+
+                        case 3:
+                            System.out.println("Mostrando Chamados");
+                            chamado.mostraChamados();
+                            break;
+
+                        case 4:
+                            break;
+                    }
+                } while (operacao!=4);
+            }
         } while (ope!=0);
     }
 }
